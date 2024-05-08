a50aef7e4d1a1741d60e702c0535d0031aba4734
Baitur Tashbaev <tashbaevvb@gmail.com>
2024-05-09 03:20:31 +0600

    books and bookDetails with some bugs

--- src/components/content/library/BookDetail.jsx
+++ src/components/content/library/BookDetail.jsx
@@ -1,6 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import "./BookDetail.css";
+import Navbar from "../../navbar/Navbar";
+// import Footer from "../../footer/Footer";
+import Book from "./Ch_ai.jpg";
 
 function BookDetail() {
   const { bookId } = useParams();
@@ -9,10 +13,12 @@ function BookDetail() {
   useEffect(() => {
     const fetchBook = async () => {
       try {
-        const response = await axios.get(`http://localhost:8086/book/getById/${bookId}`);
+        const response = await axios.get(
+          `http://localhost:8086/book/getById/${bookId}`
+        );
         setBook(response.data);
       } catch (error) {
-        console.error('Error fetching book details:', error);
+        console.error("Error fetching book details:", error);
       }
     };
 
@@ -24,13 +30,26 @@ function BookDetail() {
   }
 
   return (
-    <div>
-      <h2>{book.title}</h2>
-      <p>{book.description}</p>
-      <p>Author: {book.author}</p>
-      <p>Level: {book.level.levelName}</p>
-      <p>{book.content}</p>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <div className="book-header">
+          <img src={Book} alt="suka" />
+          <div className="book-info">
+            <h1>
+              {book.title} - {book.level.levelName}
+            </h1>
+            <p>Author: {book.author}</p>
+          </div>
+        </div>
+        <div className="book-content">
+          <p>{book.description}</p>
+          <br />
+          <p>{book.content}</p>
+        </div>
+      </div>
+      {/* <Footer /> */}
+    </>
   );
 }
 
--- src/components/content/library/Books.jsx
+++ src/components/content/library/Books.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./books.css";
+import "./Books.css";
 import Navbar from "../../navbar/Navbar";
 // import Footer from "../../footer/Footer";
 import Book from "./Ch_ai.jpg";
