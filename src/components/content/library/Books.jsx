50c9de1054e72dbf5264688eb276d8228a4ab290
Baitur Tashbaev <tashbaevvb@gmail.com>
2024-05-16 16:10:02 +0600

    blya

--- src/components/content/library/Books.jsx
+++ src/components/content/library/Books.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./books.css";
-import Navbar from "../navbar/Navbar";
+import Navbar from "../../navbar/Navbar";
 
 function Books() {
   const [books, setBooks] = useState([]);
