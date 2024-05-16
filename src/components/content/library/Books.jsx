372abc20881ed7542a5a6538335e8401403d9aac
Baitur Tashbaev <tashbaevvb@gmail.com>
2024-05-16 16:03:41 +0600

    books updated

--- src/components/content/library/Books.jsx
+++ src/components/content/library/Books.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./Books.css";
+import "./books.css";
 import Navbar from "../../navbar/Navbar";
 
 function Books() {
