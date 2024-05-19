380bb72f747ddd6072ed7bdea994cc2fdab1341f
Baitur Tashbaev <tashbaevvb@gmail.com>
2024-05-19 10:44:50 +0600

    MovieList(wie Books), MovieDetail are updated with design/dynamish, closeIcon

--- src/components/content/movies/MovieDetail.jsx
+++ src/components/content/movies/MovieDetail.jsx
@@ -1,49 +1,47 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import './MovieDetail.css';  // Import the CSS file
+import "./MovieDetail.css";
 
 function MovieDetail() {
   const { movieId } = useParams();
   const [movie, setMovie] = useState(null);
   const [videoSrc, setVideoSrc] = useState(null);
-{/* типа локально задал */}
-  // useEffect(() => {
-    // const fetchMovie = async () => {
-    //   try {
-    //     const response = await axios.get(`http://localhost:8086/movie/getById/${movieId}`);
-    //     setMovie(response.data);
-    //     if (response.data.filePath) {
-    //       setVideoSrc(`http://localhost:8086${response.data.filePath}`);
-    //     } else {
-    //       setVideoSrc("https://media.w3.org/2010/05/sintel/trailer_hd.mp4");
-    //     }
-    //   } catch (error) {
-    //     console.error("Error fetching movie details:", error);
-    //     setVideoSrc("https://media.w3.org/2010/05/sintel/trailer_hd.mp4");
-    //   }
-    // };
 
-  //   fetchMovie();
+  useEffect(() => {
+    const fetchMovie = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8086/movie/getById/${movieId}`
+        );
+        setMovie(response.data);
+        setVideoSrc(`http://localhost:8086${movie.filePath}`);
+      } catch (error) {
+        console.error("Error fetching movie details:", error);
+        setVideoSrc("https://media.w3.org/2010/05/sintel/trailer_hd.mp4");
+      }
+    };
 
-  //   const fallbackTimer = setTimeout(() => {
-  //     if (!videoSrc) {
-  //       setVideoSrc("https://media.w3.org/2010/05/sintel/trailer_hd.mp4");
-  //     }
-  //   }, 5 * 60 * 1000); // 5 minutes in milliseconds
+    fetchMovie();
 
-  //   return () => clearTimeout(fallbackTimer);
-  // }, [movieId]);
+    const fallbackTimer = setTimeout(() => {
+      if (!videoSrc) {
+        setVideoSrc("https://media.w3.org/2010/05/sintel/trailer_hd.mp4");
+      }
+    }, 5 * 60 * 1000); // 5 minutes in milliseconds
 
-  // if (!movie) {
-  //   return <div>Loading...</div>;
-  // }
+    return () => clearTimeout(fallbackTimer);
+  }, [movieId]);
+
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="video-container">
       <video controls className="video-player">
         <source
-          src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+          src={`http://localhost:8086${movie.filePath}`}
           type="video/mp4"
         />
         Your browser does not support the video tag.
--- src/components/content/movies/MoviesList.jsx
+++ src/components/content/movies/MoviesList.jsx
@@ -2,21 +2,13 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Navbar from "../../navbar/Navbar";
-import './MoviesList.css';  // Import the CSS file
+import "./MoviesList.css";
+import closeIcon from "../../../assets/close.png";
 
 function MoviesList() {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Inception",
-      country: "USA",
-      description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
-      posterUrl: "https://example.com/inception.jpg",
-      level: { levelName: "Advanced" }
-    }
-  ]);
+  const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
-  const [email, setEmail] = useState("example@example.com");
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -43,8 +35,19 @@ function MoviesList() {
     fetchData();
   }, []);
 
-  const handleMovieClick = (movie) => {
-    setSelectedMovie(movie.id === selectedMovie?.id ? null : movie);
+  const handleMovieClick = async (movieId) => {
+    if (selectedMovie && selectedMovie.id === movieId) {
+      setSelectedMovie(null);
+    } else {
+      try {
+        const response = await axios.get(
+          `http://localhost:8086/movie/getById/${movieId}`
+        );
+        setSelectedMovie(response.data);
+      } catch (error) {
+        console.error("Error fetching movie details:", error);
+      }
+    }
   };
 
   const handleClose = () => {
@@ -54,16 +57,23 @@ function MoviesList() {
   return (
     <div>
       <Navbar email={email} />
-      <div className={`movies-container ${selectedMovie ? 'blur-background' : ''}`}>
-        <h2>Movies List</h2>
+      <div
+        className={`movies-container ${selectedMovie ? "blur-background" : ""}`}
+      >
+        <h1>Filmen</h1>
         <ul className="movies-list">
           {movies.map((movie) => (
             <li key={movie.id} className="movie-item">
-              <div onClick={() => handleMovieClick(movie)} className="movie-link">
-                <img src={movie.posterUrl} alt={movie.title} className="movie-poster" />
-                <div className="movie-details">
-                  <h3 className="movie-title">{movie.title}</h3>
-                </div>
+              <div
+                onClick={() => handleMovieClick(movie.id)}
+                className="movie-link"
+              >
+                <img
+                  src={`http://localhost:8086/${movie.imgPath}`}
+                  alt={movie.title}
+                  className="movie-poster"
+                />
+                <div className="movie-title">{movie.title}</div>
               </div>
             </li>
           ))}
@@ -72,14 +82,33 @@ function MoviesList() {
       {selectedMovie && (
         <div className="movie-modal">
           <div className="movie-modal-content">
-            <button className="close-button" onClick={handleClose}>×</button>
-            <img src={selectedMovie.posterUrl} alt={selectedMovie.title} className="movie-modal-poster" />
+            <img
+              src={closeIcon}
+              alt="Clos"
+              className="close-sidebar-icon"
+              onClick={handleClose}
+            />
+            <img
+              src={`http://localhost:8086/${selectedMovie.imgPath}`}
+              alt={selectedMovie.title}
+              className="movie-modal-poster"
+            />
             <div className="movie-modal-details">
-              <h2>{selectedMovie.title}</h2>
+              <h2 id="titleLevel">
+                {selectedMovie.title} - {selectedMovie.level.levelName}
+              </h2>
               <p className="movie-country">Country: {selectedMovie.country}</p>
+              <p className="movie-level">
+                Level: {selectedMovie.level.levelName}
+              </p>
               <p className="movie-description">{selectedMovie.description}</p>
-              {selectedMovie.level && <p className="movie-level">Level: {selectedMovie.level.levelName}</p>}
-              <Link to={`/movie/play/${selectedMovie.id}`} className="play-button-modal">▶ Play</Link>
+
+              <Link
+                to={`/movie/${selectedMovie.id}`}
+                className="play-button-modal"
+              >
+                Play
+              </Link>
             </div>
           </div>
         </div>
